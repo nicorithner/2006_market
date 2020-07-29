da34fc9e095c5fc32156544b6ba490ba9f8096bf
Nico Rithner <nicorithner@gmail.com>
2020-07-29 09:14:17 -0600

    Add vendor class and initial tests. test it exists and attributes

---
+++ lib/vendor.rb
@@ -0,0 +1,9 @@
+class Vendor
+  attr_reader :name, :inventory
+  def initialize(name, inventory={})
+    @name = name
+    @inventory = inventory
+  end
+
+
+end
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -2,17 +2,21 @@ require "minitest/autorun"
 require "minitest/pride"
 require "pry"
 require "./lib/item"
+require "./lib/vendor"
 
 class VendorTest < Minitest::Test 
   def test_it_exists
-    vendor = Vendor.new
+    vendor = Vendor.new("Rocky Mountain Fresh")
 
     assert_instance_of Vendor, vendor
   end
 
-  def test_it_has_attributes
+  def test_it_has_attributes_and_starts_without_inventory
+    vendor = Vendor.new("Rocky Mountain Fresh")
 
-    assert_equal ,
+    assert_equal "Rocky Mountain Fresh", vendor.name
+    expected = {}
+    assert_equal expected, vendor.inventory
   end
 
 end
