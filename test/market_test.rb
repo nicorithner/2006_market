09d47faa19120a540f10cb74ffc3503374a5de15
Nico Rithner <nicorithner@gmail.com>
2020-07-29 10:58:49 -0600

    Add set up for iteration 3

--- test/market_test.rb
+++ test/market_test.rb
@@ -117,4 +117,53 @@ class MarketTest < Minitest::Test
     assert_equal 345, vendor2.potential_revenue
     assert_equal 48.75, vendor3.potential_revenue
   end
+
+  def test_it_can_do_total_inventory
+    market = Market.new("South Pearl Street Farmers Market")   
+ 
+    item1 = Item.new({name: "Peach", price: "$0.75"})
+    item2 = Item.new({name: "Tomato", price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+
+    vendor1.stock(item1, 35)    
+    vendor1.stock(item2, 7)   
+
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+
+    vendor2.stock(item4, 50)    
+    vendor2.stock(item3, 25) 
+
+    vendor3 = Vendor.new("Palisade Peach Shack")
+
+    vendor3.stock(item1, 65)  
+    vendor3.stock(item3, 10)
+
+    market.add_vendor(vendor1)    
+    market.add_vendor(vendor2)    
+    market.add_vendor(vendor3)    
+    
+expected = {
+    item1 => {
+      quantity: 100,
+      vendors: [#<Vendor:0x00007fe1348a1160...>, #<Vendor:0x00007fe134910650...>]
+    },
+    item2 => {
+      quantity: 7,
+      vendors: [#<Vendor:0x00007fe1348a1160...>]
+    },
+    item3 => {
+      quantity: 50,
+      vendors: [#<Vendor:0x00007fe1349bed40...>]
+    },
+    item4=> {
+      quantity: 35,
+      vendors: [#<Vendor:0x00007fe1349bed40...>, #<Vendor:0x00007fe134910650...>]
+    },
+  }
+market.total_inventory
+  end
+
 end
