37ff6d3bad164108932fc92ad40d3eb8684c0a8b
Nico Rithner <nicorithner@gmail.com>
2020-07-29 11:02:53 -0600

    Add set up for test it can do total inventory

--- test/market_test.rb
+++ test/market_test.rb
@@ -120,7 +120,7 @@ class MarketTest < Minitest::Test
 
   def test_it_can_do_total_inventory
     market = Market.new("South Pearl Street Farmers Market")   
- 
+
     item1 = Item.new({name: "Peach", price: "$0.75"})
     item2 = Item.new({name: "Tomato", price: "$0.50"})
     item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
@@ -145,25 +145,25 @@ class MarketTest < Minitest::Test
     market.add_vendor(vendor2)    
     market.add_vendor(vendor3)    
     
-expected = {
-    item1 => {
-      quantity: 100,
-      vendors: [#<Vendor:0x00007fe1348a1160...>, #<Vendor:0x00007fe134910650...>]
-    },
-    item2 => {
-      quantity: 7,
-      vendors: [#<Vendor:0x00007fe1348a1160...>]
-    },
-    item3 => {
-      quantity: 50,
-      vendors: [#<Vendor:0x00007fe1349bed40...>]
-    },
-    item4=> {
-      quantity: 35,
-      vendors: [#<Vendor:0x00007fe1349bed40...>, #<Vendor:0x00007fe134910650...>]
-    },
-  }
-market.total_inventory
+    expected = {
+        item1 => {
+          quantity: 100,
+          vendors: [vendor1, vendor2]
+        },
+        item2 => {
+          quantity: 7,
+          vendors: [vendor1]
+        },
+        item3 => {
+          quantity: 50,
+          vendors: [vendor2, vendor3]
+        },
+        item4=> {
+          quantity: 35,
+          vendors: [vendor2]
+        },
+      }
+    assert_equal expected, market.total_inventory
   end
 
 end
