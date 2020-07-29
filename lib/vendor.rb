bc8bf468fac57d0f149a939fce64ae11d585cf41
Nico Rithner <nicorithner@gmail.com>
2020-07-29 10:09:29 -0600

    Complete iteration 1

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,9 +1,26 @@
 class Vendor
   attr_reader :name, :inventory
-  def initialize(name, inventory={})
+  def initialize(name)
     @name = name
-    @inventory = inventory
+    @inventory = Hash.new{(0)}
+  end
+  def add_item(item, quantity)
+    @inventory[item] = quantity
+# {:quantity => quantity}
   end
 
+  def stock(item, quantity)
+    if  @inventory[item] == 0
+      @inventory[item] = quantity
+    else
+      @inventory[item] += quantity
+    end
+# binding.pry
+  end
+
+  def check_stock(item)
+    @inventory[item]
+# [:quantity]
+  end
 
 end
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -19,4 +19,31 @@ class VendorTest < Minitest::Test
     assert_equal expected, vendor.inventory
   end
 
+  def test_it_can_check_stock_and_have_inventory
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    vendor.add_item(item1, 0)
+# binding.pry
+    
+    assert_equal 0, vendor.check_stock(item1)
+
+    vendor.stock(item1, 30)
+
+    expected = {item1 => 30}
+    assert_equal expected, vendor.inventory
+    assert_equal 30, vendor.check_stock(item1)
+    
+    vendor.stock(item1, 25)
+
+    assert_equal 55, vendor.check_stock(item1)
+    
+    vendor.stock(item2, 12)
+
+    expected = {item1 => 55, item2 => 12}
+    assert_equal expected, vendor.inventory
+  end
+
 end
